10969cee1521b4c788cbb9fc677d37a5dd62a62e
Elena <lenhenlady@gmail.com>
2020-11-04 12:48:18 +0300

    vendor folder with fonts and normalaize added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,12 @@
 import Header from '../Header/Header';
+import Main from './Main/Main';
 
 function App() {
   return (
     <div className="root">
       <Header></Header>
+      <Main></Main>
+
     </div>
   );
 }
